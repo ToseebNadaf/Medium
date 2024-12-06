82ff8991fe2532e6ae41bc6da584b739e14674a7
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-06 19:10:10 +0530

    Add middleware for request handling and authentication

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,13 +1,38 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
 
 export const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
+  Variables: {
+    jwtPayload: string;
+  };
 }>();
+
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  try {
+    const user = await verify(authHeader, c.env.JWT_SECRET);
+    if (user) {
+      c.set("jwtPayload", user.id);
+      await next();
+    } else {
+      c.status(403);
+      return c.json({
+        message: "Not logged in",
+      });
+    }
+  } catch (error) {
+    c.status(403);
+    return c.json({
+      message: "Not logged in",
+    });
+  }
+});
 
 blogRouter.post("/", async (c) => {
   const prisma = new PrismaClient({
@@ -15,24 +40,25 @@ blogRouter.post("/", async (c) => {
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
+  const authorId = c.get("jwtPayload");
 
   try {
     const blog = await prisma.post.create({
       data: {
         title: body.title,
         content: body.content,
-        authorId: body.authorId,
+        authorId: Number(authorId),
       },
     });
 
     return c.json({
-      message: "Blog created",
+      message: "Blog created successfully.",
       id: blog.id,
     });
   } catch (error) {
-    c.status(403);
+    c.status(500);
     return c.json({
-      message: "Post not created",
+      message: "An unexpected error occurred while creating the blog post.",
     });
   }
 });
