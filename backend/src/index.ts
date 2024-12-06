5238b1ec002e41754b7dfa56c29287fd265cd645
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-06 17:53:53 +0530

    Integrate Prisma for database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,18 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
 app.post("/api/v1/signup", (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  
   return c.text("Signup route");
 });
 
@@ -12,7 +22,6 @@ app.post("/api/v1/signin", (c) => {
 
 app.get("/api/v1/blog/:id", (c) => {
   const id = c.req.param("id");
-  console.log(id);
   return c.text("Get blog route");
 });
 
