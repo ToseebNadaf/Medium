c1a15135f19d7aa0bdbed03d836421c2ea3debb3
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-06 18:19:15 +0530

    Implement JWT-based authentication for user

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,36 +1,75 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
 }>();
 
-app.post("/api/v1/signup", (c) => {
+app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  
-  return c.text("Signup route");
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    const jwt = sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({
+      message: "User created successfully.",
+      jwt,
+    });
+  } catch (error) {
+    c.status(500);
+    return c.json({
+      message: "An unexpected error occurred.",
+    });
+  }
 });
 
-app.post("/api/v1/signin", (c) => {
-  return c.text("Signin route");
-});
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.get("/api/v1/blog/:id", (c) => {
-  const id = c.req.param("id");
-  return c.text("Get blog route");
-});
+  const body = await c.req.json();
 
-app.post("/api/v1/blog", (c) => {
-  return c.text("Create blog");
-});
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+      },
+    });
 
-app.put("/api/v1/blog", (c) => {
-  return c.text("update blog");
+    if (!user) {
+      c.status(404);
+      return c.json({
+        message: "User not found. Please sign up.",
+      });
+    }
+
+    const jwt = sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({
+      message: "Signed in successfully.",
+      jwt,
+    });
+  } catch (error) {
+    c.status(500);
+    return c.json({
+      message: "An unexpected error occurred.",
+    });
+  }
 });
 
 export default app;
