01a1d3003aabb636b28ce44cafcb9564b8823517
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-07 19:28:27 +0530

    Add single blog view page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
-import Blog from "./pages/Blog";
 import Publish from "./pages/Publish";
 import { Blogs } from "./pages/Blogs";
+import { Blog } from "./pages/Blog";
 function App() {
   return (
     <>
