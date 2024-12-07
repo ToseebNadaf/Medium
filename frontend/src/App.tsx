f79717c1849966b00cc40e055952d778e31fa4ec
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-07 19:09:06 +0530

    Add blog listing page to show all blogs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Blog from "./pages/Blog";
+import Publish from "./pages/Publish";
+import { Blogs } from "./pages/Blogs";
 function App() {
   return (
     <>
@@ -10,6 +12,8 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/publish" element={<Publish />} />
         </Routes>
       </BrowserRouter>
     </>
